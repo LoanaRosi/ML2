715ca963ca94114bbb88e274fdff06b785245391
LoanaRosi <loarosi24@gmail.com>
2021-08-12 14:35:37 -0300

    pregreso

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,17 +4,27 @@ const path = require('path');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const toThousand = require('../utils/toThousand')
+const finalPrice = require('../utils/finalPrice')
 
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		return res.render('products', {
+			products,
+			finalPrice,
+			toThousand
+		})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		let product = products.find(product => product.id === +req.params.id)
+		return res.render('detail', {
+			product,
+			finalPrice,
+			toThousand
+		})
 	},
 
 	// Create - Form to create
